37538973b325fa9713963920b1bf9ff3abae717f
Willy <william.murray1995@gmail.com>
2023-03-14 22:34:47 +1300

    Bot now responds to !conspiracy

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,7 @@
 import discord
 import subprocess
 from secrets_1 import DISCORD_API_KEY
-
-permissions_int = 10256
+from main import get_conspiracy_post
 
 intents = discord.Intents.default()
 intents.members = True
@@ -10,7 +9,7 @@ intents.presences = True
 intents.message_content = True
 client = discord.Client(intents=intents)
 
-prefix = "!" # Change the prefix to "!"
+prefix = "!"
 
 @client.event
 async def on_ready():
@@ -20,8 +19,9 @@ async def on_ready():
 async def on_message(message):
     print('Message received:', message.content) # Add this line to check if messages are being received by the bot
 
-    if message.content.startswith(prefix + 'hello'): # Use the prefix variable here
-        await message.channel.send('Hello!')
+    if message.content.lower().startswith(prefix + 'conspiracy'): # Use the prefix variable here
+        conspiracy_post = get_conspiracy_post()
+        await message.channel.send(conspiracy_post)
 
 client.run(DISCORD_API_KEY)
 
--- main.py
+++ main.py
@@ -2,31 +2,30 @@ import feedparser
 import random
 from bs4 import BeautifulSoup
 
-# Parse the RSS feed
-rss_url = "https://www.reddit.com/r/conspiracytheories.rss"
-feed = feedparser.parse(rss_url)
+def get_conspiracy_post():
+    # Parse the RSS feed
+    rss_url = "https://www.reddit.com/r/conspiracytheories.rss"
+    feed = feedparser.parse(rss_url)
 
-# Get a random post from the feed
-post = random.choice(feed.entries)
+    # Get a random post from the feed
+    post = random.choice(feed.entries)
 
-# Extract the title and link from the post
-title = post.title
-link = post.link
+    # Extract the title and link from the post
+    title = post.title
+    link = post.link
 
-rss_striped_link = link[:-1] + ".rss"
+    rss_striped_link = link[:-1] + ".rss"
 
-feed2 = feedparser.parse(rss_striped_link) #parse the feed from the individual link selected
-content = feed2.entries[0].content[0].value #extract content for first entry
+    feed2 = feedparser.parse(rss_striped_link) #parse the feed from the individual link selected
+    content = feed2.entries[0].content[0].value #extract content for first entry
 
-soup = BeautifulSoup(content, "html.parser")
-p_tags = soup.find_all("p")
-if p_tags:
-    first_p_tag = p_tags[0]
-    content_text = first_p_tag.get_text()
-else:
-    content_text = "N/A (probably because there was only an image)"
+    soup = BeautifulSoup(content, "html.parser")
+    p_tags = soup.find_all("p")
+    if p_tags:
+        first_p_tag = p_tags[0]
+        content_text = first_p_tag.get_text()
+    else:
+        content_text = "N/A (probably because there was only an image)"
 
-# Print the results
-print("Title:", title)
-print("Description:", content_text)
-print("Link:", link)
+    # Return the results
+    return f"Title: {title}\nDescription: {content_text}\nLink: {link}"
